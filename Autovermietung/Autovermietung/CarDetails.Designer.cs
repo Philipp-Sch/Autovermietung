1fcb8fdcbe3f1b2a3590d6a170ba50bbefbb3b19
Philipp Schiffbauer <freackyphil@gmail.com>
2017-11-28 09:15:10 +0100

    Branch erstellt. Oberfläche erstellt

---
+++ Autovermietung/Autovermietung/CarDetails.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Autovermietung
+{
+    partial class CarDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.producerLabel = new System.Windows.Forms.Label();
+            this.producerTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.modelLabel = new System.Windows.Forms.Label();
+            this.powerTextBox = new System.Windows.Forms.TextBox();
+            this.powerLabel = new System.Windows.Forms.Label();
+            this.seatsTextBox = new System.Windows.Forms.TextBox();
+            this.seatsLabel = new System.Windows.Forms.Label();
+            this.trunkTextBox = new System.Windows.Forms.TextBox();
+            this.trunkLabel = new System.Windows.Forms.Label();
+            this.fuelTextBox = new System.Windows.Forms.TextBox();
+            this.fuelLabel = new System.Windows.Forms.Label();
+            this.gearTextBox = new System.Windows.Forms.TextBox();
+            this.gearLabel = new System.Windows.Forms.Label();
+            this.trailerTextBox = new System.Windows.Forms.TextBox();
+            this.trailerLabel = new System.Windows.Forms.Label();
+            this.classTextBox = new System.Windows.Forms.TextBox();
+            this.classLabel = new System.Windows.Forms.Label();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.bookingButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 28);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(126, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // producerLabel
+            // 
+            this.producerLabel.AutoSize = true;
+            this.producerLabel.Location = new System.Drawing.Point(12, 87);
+            this.producerLabel.Name = "producerLabel";
+            this.producerLabel.Size = new System.Drawing.Size(51, 13);
+            this.producerLabel.TabIndex = 2;
+            this.producerLabel.Text = "Hersteller";
+            // 
+            // producerTextBox
+            // 
+            this.producerTextBox.Location = new System.Drawing.Point(15, 103);
+            this.producerTextBox.Name = "producerTextBox";
+            this.producerTextBox.ReadOnly = true;
+            this.producerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.producerTextBox.TabIndex = 3;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Location = new System.Drawing.Point(15, 142);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.ReadOnly = true;
+            this.modelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.modelTextBox.TabIndex = 5;
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.AutoSize = true;
+            this.modelLabel.Location = new System.Drawing.Point(12, 126);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(38, 13);
+            this.modelLabel.TabIndex = 4;
+            this.modelLabel.Text = "Modell";
+            // 
+            // powerTextBox
+            // 
+            this.powerTextBox.Location = new System.Drawing.Point(15, 222);
+            this.powerTextBox.Name = "powerTextBox";
+            this.powerTextBox.ReadOnly = true;
+            this.powerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.powerTextBox.TabIndex = 9;
+            // 
+            // powerLabel
+            // 
+            this.powerLabel.AutoSize = true;
+            this.powerLabel.Location = new System.Drawing.Point(12, 205);
+            this.powerLabel.Name = "powerLabel";
+            this.powerLabel.Size = new System.Drawing.Size(47, 13);
+            this.powerLabel.TabIndex = 8;
+            this.powerLabel.Text = "Leistung";
+            // 
+            // seatsTextBox
+            // 
+            this.seatsTextBox.Location = new System.Drawing.Point(207, 103);
+            this.seatsTextBox.Name = "seatsTextBox";
+            this.seatsTextBox.ReadOnly = true;
+            this.seatsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.seatsTextBox.TabIndex = 11;
+            // 
+            // seatsLabel
+            // 
+            this.seatsLabel.AutoSize = true;
+            this.seatsLabel.Location = new System.Drawing.Point(204, 86);
+            this.seatsLabel.Name = "seatsLabel";
+            this.seatsLabel.Size = new System.Drawing.Size(52, 13);
+            this.seatsLabel.TabIndex = 10;
+            this.seatsLabel.Text = "Sitzplätze";
+            // 
+            // trunkTextBox
+            // 
+            this.trunkTextBox.Location = new System.Drawing.Point(207, 143);
+            this.trunkTextBox.Name = "trunkTextBox";
+            this.trunkTextBox.ReadOnly = true;
+            this.trunkTextBox.Size = new System.Drawing.Size(100, 20);
+            this.trunkTextBox.TabIndex = 13;
+            // 
+            // trunkLabel
+            // 
+            this.trunkLabel.AutoSize = true;
+            this.trunkLabel.Location = new System.Drawing.Point(204, 126);
+            this.trunkLabel.Name = "trunkLabel";
+            this.trunkLabel.Size = new System.Drawing.Size(58, 13);
+            this.trunkLabel.TabIndex = 12;
+            this.trunkLabel.Text = "Kofferraum";
+            // 
+            // fuelTextBox
+            // 
+            this.fuelTextBox.Location = new System.Drawing.Point(207, 183);
+            this.fuelTextBox.Name = "fuelTextBox";
+            this.fuelTextBox.ReadOnly = true;
+            this.fuelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fuelTextBox.TabIndex = 15;
+            // 
+            // fuelLabel
+            // 
+            this.fuelLabel.AutoSize = true;
+            this.fuelLabel.Location = new System.Drawing.Point(204, 166);
+            this.fuelLabel.Name = "fuelLabel";
+            this.fuelLabel.Size = new System.Drawing.Size(51, 13);
+            this.fuelLabel.TabIndex = 14;
+            this.fuelLabel.Text = "Treibstoff";
+            // 
+            // gearTextBox
+            // 
+            this.gearTextBox.Location = new System.Drawing.Point(207, 223);
+            this.gearTextBox.Name = "gearTextBox";
+            this.gearTextBox.ReadOnly = true;
+            this.gearTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gearTextBox.TabIndex = 17;
+            // 
+            // gearLabel
+            // 
+            this.gearLabel.AutoSize = true;
+            this.gearLabel.Location = new System.Drawing.Point(204, 206);
+            this.gearLabel.Name = "gearLabel";
+            this.gearLabel.Size = new System.Drawing.Size(57, 13);
+            this.gearLabel.TabIndex = 16;
+            this.gearLabel.Text = "Antriebsart";
+            // 
+            // trailerTextBox
+            // 
+            this.trailerTextBox.Location = new System.Drawing.Point(207, 263);
+            this.trailerTextBox.Name = "trailerTextBox";
+            this.trailerTextBox.ReadOnly = true;
+            this.trailerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.trailerTextBox.TabIndex = 19;
+            // 
+            // trailerLabel
+            // 
+            this.trailerLabel.AutoSize = true;
+            this.trailerLabel.Location = new System.Drawing.Point(204, 246);
+            this.trailerLabel.Name = "trailerLabel";
+            this.trailerLabel.Size = new System.Drawing.Size(97, 13);
+            this.trailerLabel.TabIndex = 18;
+            this.trailerLabel.Text = "Anhängerkupplung";
+            // 
+            // classTextBox
+            // 
+            this.classTextBox.Location = new System.Drawing.Point(15, 182);
+            this.classTextBox.Name = "classTextBox";
+            this.classTextBox.ReadOnly = true;
+            this.classTextBox.Size = new System.Drawing.Size(100, 20);
+            this.classTextBox.TabIndex = 7;
+            // 
+            // classLabel
+            // 
+            this.classLabel.AutoSize = true;
+            this.classLabel.Location = new System.Drawing.Point(12, 165);
+            this.classLabel.Name = "classLabel";
+            this.classLabel.Size = new System.Drawing.Size(81, 13);
+            this.classLabel.TabIndex = 6;
+            this.classLabel.Text = "Fahrzeugklasse";
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(12, 9);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(44, 13);
+            this.searchLabel.TabIndex = 0;
+            this.searchLabel.Text = "Suchen";
+            // 
+            // bookingButton
+            // 
+            this.bookingButton.Location = new System.Drawing.Point(271, 330);
+            this.bookingButton.Name = "bookingButton";
+            this.bookingButton.Size = new System.Drawing.Size(75, 23);
+            this.bookingButton.TabIndex = 20;
+            this.bookingButton.Text = "Buchen";
+            this.bookingButton.UseVisualStyleBackColor = true;
+            // 
+            // CarDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 365);
+            this.Controls.Add(this.bookingButton);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.classTextBox);
+            this.Controls.Add(this.classLabel);
+            this.Controls.Add(this.trailerTextBox);
+            this.Controls.Add(this.trailerLabel);
+            this.Controls.Add(this.gearTextBox);
+            this.Controls.Add(this.gearLabel);
+            this.Controls.Add(this.fuelTextBox);
+            this.Controls.Add(this.fuelLabel);
+            this.Controls.Add(this.trunkTextBox);
+            this.Controls.Add(this.trunkLabel);
+            this.Controls.Add(this.seatsTextBox);
+            this.Controls.Add(this.seatsLabel);
+            this.Controls.Add(this.powerTextBox);
+            this.Controls.Add(this.powerLabel);
+            this.Controls.Add(this.modelTextBox);
+            this.Controls.Add(this.modelLabel);
+            this.Controls.Add(this.producerTextBox);
+            this.Controls.Add(this.producerLabel);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "CarDetails";
+            this.Text = "Fahrzeugdetails";
+            this.Load += new System.EventHandler(this.CarDetailscs_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label producerLabel;
+        private System.Windows.Forms.TextBox producerTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.Label modelLabel;
+        private System.Windows.Forms.TextBox powerTextBox;
+        private System.Windows.Forms.Label powerLabel;
+        private System.Windows.Forms.TextBox seatsTextBox;
+        private System.Windows.Forms.Label seatsLabel;
+        private System.Windows.Forms.TextBox trunkTextBox;
+        private System.Windows.Forms.Label trunkLabel;
+        private System.Windows.Forms.TextBox fuelTextBox;
+        private System.Windows.Forms.Label fuelLabel;
+        private System.Windows.Forms.TextBox gearTextBox;
+        private System.Windows.Forms.Label gearLabel;
+        private System.Windows.Forms.TextBox trailerTextBox;
+        private System.Windows.Forms.Label trailerLabel;
+        private System.Windows.Forms.TextBox classTextBox;
+        private System.Windows.Forms.Label classLabel;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button bookingButton;
+    }
+}
---
+++ Autovermietung/Autovermietung/CarDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autovermietung
+{
+    public partial class CarDetails : Form
+    {
+        public CarDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void CarDetailscs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
